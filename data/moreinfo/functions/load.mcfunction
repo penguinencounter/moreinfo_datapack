ad7ee6b888e5a1c4d702d052041347934cf356f4
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2021-01-22 15:51:46 -0800

    created template generator; tested, works

--- data/moreinfo/functions/load.mcfunction
+++ data/moreinfo/functions/load.mcfunction
@@ -1,2 +1,3 @@
 #alias vector box -2079934 0 -21744925
 #alias vector sign -2079934 0 -21744926
+forceload add -2079934 -21744925
---
+++ data/moreinfo/generate_files.py
@@ -0,0 +1,12 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
+env = Environment(
+    loader=FileSystemLoader('C:\\Users\\Miles\\bin\\MultiMC\\instances\\1.16.2 FABRIC\\.minecraft\\saves\\farm team\\datapacks\\moreinfo\\data\\moreinfo\\templates'),
+    autoescape=select_autoescape([])
+)
+
+slot_processing = env.get_template('slot_template.mcfunction.j2')
+print(os.listdir('.'))
+for x in range(0, 36):
+    with open(f'data/moreinfo/functions/slot_processing/slot_{x}.mcfunction', 'w') as f:
+        f.write(slot_processing.render(slot_number=x))
